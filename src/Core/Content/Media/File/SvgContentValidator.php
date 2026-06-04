3b516b11442ca8b1109f0f34d3bdf370b6e7b589
David Neustadt <d.neustadt@shopware.com>
2026-06-04 13:35:55 +0200

    fix: make svg violation exceptions more verbose (#17194)

--- src/Core/Content/Media/File/SvgContentValidator.php
+++ src/Core/Content/Media/File/SvgContentValidator.php
@@ -5,6 +5,8 @@ namespace Shopware\Core\Content\Media\File;
 use Shopware\Core\Content\Media\MediaException;
 use Shopware\Core\Framework\Log\Package;
 use Shopware\Core\Framework\Plugin\Exception\DecorationPatternException;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationList;
 
 #[Package('discovery')]
 class SvgContentValidator extends AbstractFileContentValidator
@@ -18,6 +20,12 @@ class SvgContentValidator extends AbstractFileContentValidator
     private const XLINK_NAMESPACE = 'http://www.w3.org/1999/xlink';
     private const XMLNS_NAMESPACE = 'http://www.w3.org/2000/xmlns/';
     private const XML_NAMESPACE = 'http://www.w3.org/XML/1998/namespace';
+    private const DISALLOWED_NODE_TYPE = 'Node types not allowed';
+    private const DISALLOWED_ELEMENT = 'Elements not allowed';
+    private const DISALLOWED_EVENT_HANDLER_ATTRIBUTE = 'Event handler attributes not allowed';
+    private const DISALLOWED_ATTRIBUTE = 'Attributes not allowed';
+    private const DISALLOWED_EXTERNAL_REFERENCE = 'External references not allowed';
+    private const DISALLOWED_EXTERNAL_STYLE_REFERENCE = 'External style references not allowed';
 
     /**
      * @var list<string>
@@ -34,6 +42,8 @@ class SvgContentValidator extends AbstractFileContentValidator
      */
     private readonly array $allowedReferenceAttributes;
 
+    private readonly ConstraintViolationList $violations;
+
     /**
      * @internal
      *
@@ -49,6 +59,7 @@ class SvgContentValidator extends AbstractFileContentValidator
         $this->allowedElements = $this->normalizeAllowlist($allowedElements);
         $this->allowedAttributes = $this->normalizeAllowlist($allowedAttributes);
         $this->allowedReferenceAttributes = $this->normalizeAllowlist($allowedReferenceAttributes);
+        $this->violations = new ConstraintViolationList();
     }
 
     public function getDecorated(): AbstractFileContentValidator
@@ -67,6 +78,10 @@ class SvgContentValidator extends AbstractFileContentValidator
             return;
         }
 
+        while ($this->violations->count() > 0) {
+            $this->violations->offsetUnset(0);
+        }
+
         $previousErrorHandling = $this->captureLibxmlErrors();
 
         try {
@@ -81,6 +96,10 @@ class SvgContentValidator extends AbstractFileContentValidator
             if ($this->hasCollectedLibxmlErrors()) {
                 throw MediaException::invalidFile(self::PARSE_ERROR_MESSAGE);
             }
+
+            if ($this->violations->count() > 0) {
+                throw MediaException::invalidFile($this->getViolationsMessage());
+            }
         } finally {
             $this->restoreLibxmlErrorHandling($previousErrorHandling);
         }
@@ -92,7 +111,7 @@ class SvgContentValidator extends AbstractFileContentValidator
 
         while ($reader->read()) {
             if ($this->isDisallowedNodeType($reader->nodeType)) {
-                $this->rejectActiveContent();
+                $this->buildViolation(self::DISALLOWED_NODE_TYPE, $reader->name);
             }
 
             if ($reader->nodeType !== \XMLReader::ELEMENT) {
@@ -136,7 +155,7 @@ class SvgContentValidator extends AbstractFileContentValidator
     private function assertElementAllowed(string $elementName): void
     {
         if (!\in_array($elementName, $this->allowedElements, true)) {
-            $this->rejectActiveContent();
+            $this->buildViolation(self::DISALLOWED_ELEMENT, $elementName);
         }
     }
 
@@ -160,20 +179,20 @@ class SvgContentValidator extends AbstractFileContentValidator
         $attributeName = mb_strtolower($reader->name);
 
         if ($this->isEventHandlerAttribute($attributeName)) {
-            $this->rejectActiveContent();
+            $this->buildViolation(self::DISALLOWED_EVENT_HANDLER_ATTRIBUTE, $attributeName);
         }
 
         if (!$this->isAllowedAttribute($reader, $attributeName)) {
-            $this->rejectActiveContent();
+            $this->buildViolation(self::DISALLOWED_ATTRIBUTE, $attributeName);
         }
 
         $isReferenceAttribute = \in_array($attributeName, $this->allowedReferenceAttributes, true);
         if ($isReferenceAttribute && $this->isExternalReference($reader->value)) {
-            $this->rejectActiveContent();
+            $this->buildViolation(self::DISALLOWED_EXTERNAL_REFERENCE, $attributeName);
         }
 
         if ($this->containsExternalStyleReference($reader->value)) {
-            $this->rejectActiveContent();
+            $this->buildViolation(self::DISALLOWED_EXTERNAL_STYLE_REFERENCE, $attributeName);
         }
     }
 
@@ -189,13 +208,35 @@ class SvgContentValidator extends AbstractFileContentValidator
         }
 
         if ($this->containsExternalStyleReference($reader->readInnerXml())) {
-            $this->rejectActiveContent();
+            $this->buildViolation(self::DISALLOWED_EXTERNAL_STYLE_REFERENCE, $elementName);
         }
     }
 
-    private function rejectActiveContent(): never
+    private function buildViolation(string $violation, string $invalidValue): void
     {
-        throw MediaException::invalidFile(self::ACTIVE_CONTENT_MESSAGE);
+        $this->violations->add(new ConstraintViolation(
+            $violation,
+            '',
+            [],
+            null,
+            '',
+            $invalidValue
+        ));
+    }
+
+    private function getViolationsMessage(): string
+    {
+        $valuesByMessage = [];
+        foreach ($this->violations as $violation) {
+            $valuesByMessage[(string) $violation->getMessage()][] = $violation->getInvalidValue();
+        }
+
+        $lines = [self::ACTIVE_CONTENT_MESSAGE];
+        foreach ($valuesByMessage as $message => $values) {
+            $lines[] = \sprintf('%s: %s', $message, implode(', ', array_unique($values)));
+        }
+
+        return implode(\PHP_EOL, $lines);
     }
 
     /**
